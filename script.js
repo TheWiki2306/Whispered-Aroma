bb4edc3a8fe2e05ddfdef471f8d3316d088b4ea3
olusegun <olusegundavids1@gmail.com>
2024-08-22 16:31:32 +0100

    On branch master

---
+++ script.js
@@ -0,0 +1,31 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    })
+})
+
+
+
+const hiddenElement =  document.querySelectorAll('.hidden');
+hiddenElement.forEach((el) => observer.observe(el));
+
+const shownElement =  document.querySelectorAll('.hidden-2');
+shownElement.forEach((el) => observer.observe(el));
+
+
+function showSideBar() {
+    const sidebar = document.querySelectorAll(".sidebar");
+    sidebar.style.display = "flex";
+    s
+}
+
+
+function hideSideBar(){
+    const hide = document.querySelector(".sidebar");
+    hide.style.display = "none";
+}
